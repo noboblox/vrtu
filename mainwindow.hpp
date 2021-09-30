dffdedffe6ad864b06ffe5567c0797be4488446c
blox <32567857+noboblox@users.noreply.github.com>
2021-09-30 20:46:09 +0200

    io_context inside dedicated networking thread

--- mainwindow.hpp
+++ mainwindow.hpp
@@ -1,10 +1,15 @@
 #ifndef MAINWINDOW_HPP_
 #define MAINWINDOW_HPP_
 
-#include <QMainWindow>
+#include <qmainwindow.h>
+#include <qstring.h>
 
-namespace Ui {
-class MainWindow;
+#include <boost/asio/io_context.hpp>
+#include <thread>
+
+namespace Ui 
+{
+    class MainWindow;
 }
 
 class MainWindow : public QMainWindow
@@ -16,7 +21,22 @@ public:
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+    boost::asio::io_context mContext;
+    std::thread mNetworkThread; // Thread handling all non-qt tcp connections
+    QString mThreadErrorMsg, mThreadErrorInfoMsg;
+
+    Ui::MainWindow* ui;
+
+    void NetworkingThreadFunction() noexcept;
+
+private slots: 
+    /// Process error reason from network thread. The event is forced to be send over event queue, as only the main thread is allowed to create widgets
+    /// The message is stored inside mThreadErrorMsg and mThreadErrorInfoMsg. The network thread writes the data before executing the signal
+    void ReceiveThreadAbort();
+
+signals:
+    /// Called by the network thread to signal unexpected termination to the main (GUI) thread
+    void SendThreadAbort();
 };
 
 #endif // MAINWINDOW_HPP
